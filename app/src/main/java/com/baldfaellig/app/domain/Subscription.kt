748f4b3dadff328d1b6778a160fa01ce3fe76961
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-09-07 20:10:00 +0200

    feat: add domain package and Subscription domain model

---
+++ app/src/main/java/com/baldfaellig/app/domain/Subscription.kt
@@ -0,0 +1,38 @@
+package com.baldfaellig.app.domain
+
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class Subscription(
+    val name: String,
+    val amount: BigDecimal,
+    val currency: String,
+    val interval: BillingInterval,
+    val nextPayment: LocalDate,
+    val category: Category,
+    val notes: String?
+)
+
+data class BillingInterval(
+    val value: Int,
+    val unit: BillingIntervalUnit
+)
+
+enum class BillingIntervalUnit {
+    DAY,
+    MONTH,
+    YEAR,
+}
+
+enum class Category {
+    ENTERTAINMENT,
+    INSURANCE,
+    STREAMING,
+    SOFTWARE,
+    NEWS,
+    EDUCATION,
+    FOOD,
+    FINANCE,
+    FITNESS,
+    OTHER,
+}
